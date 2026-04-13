4fc93d709e2601125bb0d50e73324f022f4e0efd
Gizm0-O <datguytk4@gmail.com>
2026-04-13 15:01:38 +0000

    Narozeniny: vertikalni jmeno ve sloupci

--- fix.py
+++ fix.py
@@ -2,42 +2,8 @@ fp = './src/pages/Index.tsx'
 with open(fp, 'r', encoding='utf-8') as f:
     kod = f.read()
 
-stare = '''              backgroundImage: "url('/birthday-pattern.jpg')",
-              backgroundSize: "320px auto",
-              backgroundRepeat: "repeat",
-              backgroundPosition: "center top",
-            }}
-        />'''
-
-nove = '''              backgroundImage: "url('/birthday-pattern.jpg')",
-              backgroundSize: "320px auto",
-              backgroundRepeat: "repeat",
-              backgroundPosition: "center top",
-            }}
-          >
-            <div style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              gap: "2px",
-              pointerEvents: "none",
-            }}>
-              {BIRTHDAY_NAMES[format(day, "MM-dd")].split("").map((letter, i) => (
-                <span key={i} style={{
-                  fontFamily: "'Dancing Script', cursive",
-                  fontSize: "20px",
-                  fontWeight: "bold",
-                  color: "rgba(180, 83, 9, 0.85)",
-                  lineHeight: 1.1,
-                  textShadow: "0 0 8px rgba(255,255,255,0.9)",
-                }}>{letter}</span>
-              ))}
-            </div>
-          </div>'''
+stare = '              backgroundPosition: "center top",\n            }}\n '
+nove  = '              backgroundPosition: "center top",\n            }}\n          >\n            <div style={{\n              position: "absolute",\n              top: "50%",\n              left: "50%",\n              transform: "translate(-50%, -50%)",\n              display: "flex",\n              flexDirection: "column",\n              alignItems: "center",\n              gap: "2px",\n              pointerEvents: "none",\n            }}>\n              {BIRTHDAY_NAMES[format(day, "MM-dd")].split("").map((letter, i) => (\n                <span key={i} style={{\n                  fontFamily: "\'Dancing Script\', cursive",\n                  fontSize: "20px",\n                  fontWeight: "bold",\n                  color: "rgba(180, 83, 9, 0.85)",\n                  lineHeight: 1.1,\n                  textShadow: "0 0 8px rgba(255,255,255,0.9)",\n                }}>{letter}</span>\n              ))}\n            </div>\n          </div>\n '
 
 if stare in kod:
     kod = kod.replace(stare, nove, 1)
@@ -45,4 +11,5 @@ if stare in kod:
         f.write(kod)
     print("OK - vertikalni jmeno pridano!")
 else:
-    print("CHYBA - nenalezeno")
+    idx = kod.find('backgroundPosition: "center top"')
+    print("CHYBA:", repr(kod[idx:idx+80]))
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -935,6 +935,30 @@ const Index = () => {
               backgroundRepeat: "repeat",
               backgroundPosition: "center top",
             }}
+          >
+            <div style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "2px",
+              pointerEvents: "none",
+            }}>
+              {BIRTHDAY_NAMES[format(day, "MM-dd")].split("").map((letter, i) => (
+                <span key={i} style={{
+                  fontFamily: "'Dancing Script', cursive",
+                  fontSize: "20px",
+                  fontWeight: "bold",
+                  color: "rgba(180, 83, 9, 0.85)",
+                  lineHeight: 1.1,
+                  textShadow: "0 0 8px rgba(255,255,255,0.9)",
+                }}>{letter}</span>
+              ))}
+            </div>
+          </div>
           />
         );
       })}
